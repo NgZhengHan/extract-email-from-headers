62032259bffb23ffcd1c758b9262d1b71a0614d5
Ng Zheng Han <ng.zheng.han@gmail.com>
2024-01-05 14:36:18 +0800

    Add delegate for streamlit_helper_email_input into __init__.py
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

--- extract_email_from_http_header/__init__.py
+++ extract_email_from_http_header/__init__.py
@@ -4,5 +4,9 @@ extract-email-from-http-header.
 Utility package to help extract email from HTTP Request headers.
 """
 
+from .streamlit_helper_email_input import streamlit_helper_email_input
+
 __version__ = "0.1.0"
 __author__ = 'Ng Zheng Han'
+
+streamlit_helper_email_input = streamlit_helper_email_input
