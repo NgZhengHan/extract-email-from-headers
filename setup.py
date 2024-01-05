306bf955f5b78e822a5434a907f8fd60b2c64c66
Ng Zheng Han <ng.zheng.han@gmail.com>
2024-01-05 17:07:03 +0800

    Add MANIFEST.in to fix bug with setup.py not including VERSION Rebuild extract-email-from-http-header-0.1.0.tar.gz
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(
     url='https://github.com/NgZhengHan/extract-email-from-http-header',
     author='Ng Zheng Han',
     author_email='ng.zheng.han@gmail.com',
-    packages=['extract_email_from_http_header'],
+    packages=['extract_email_from_http_header'],    
+    package_data={"": ["*"], },
+    include_package_data=True,
     install_requires=['streamlit',                   
                       ],
 )
