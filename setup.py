1166e2965aff08aa56d90d11cc6569ca94960939
Ng Zheng Han <ng.zheng.han@gmail.com>
2024-01-05 10:38:15 +0800

    Add setup.py and VERSION
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("VERSION", "r", encoding="utf-8") as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name='extract-email-from-http-header',
+    version = version,    
+    description='Utility package to extract email from HTTP Request header.',
+    url='https://github.com/NgZhengHan/extract-email-from-http-header',
+    author='Ng Zheng Han',
+    author_email='ng.zheng.han@gmail.com',
+    license='BSD 2-clause',
+    packages=['extract-email-from-http-header'],
+    install_requires=['streamlit',                   
+                      ],
+)
