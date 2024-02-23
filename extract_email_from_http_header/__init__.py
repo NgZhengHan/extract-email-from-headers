65bc81d7aeee1e3556544d38d9d8eeb73ef81092
Ng Zheng Han <ng.zheng.han@gmail.com>
2024-02-23 17:51:32 +0800

    Remove unused code
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

--- extract_email_from_http_header/__init__.py
+++ extract_email_from_http_header/__init__.py
@@ -4,17 +4,9 @@ extract-email-from-http-header.
 Utility package to help extract email from HTTP Request headers.
 """
 
-# from .streamlit_helper_email_input import streamlit_helper_email_input
-# from .extract_email_from_headers import extract_email_from_headers
-# from .validate_email import validate_email
-
 from . import streamlit_helper_email_input
 from . import extract_email_from_headers
 from . import validate_email
 
 __version__ = "0.1.4"
 __author__ = 'Ng Zheng Han'
-
-# streamlit_helper_email_input = streamlit_helper_email_input
-# extract_email_from_headers = extract_email_from_headers
-# validate_email = validate_email
