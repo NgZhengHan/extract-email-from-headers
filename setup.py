84e2f9be4c77470731ca2ee9d80dfd4a99c87e23
Ng Zheng Han <ng.zheng.han@gmail.com>
2024-02-26 20:15:55 +0800

    Build 0.1.6
    Signed-off-by: Ng Zheng Han <ng.zheng.han@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='extract-email-from-http-header',
-    version = "0.1.5",    
+    version = "0.1.6",    
     description='Utility package to extract email from HTTP Request header.',
     url='https://github.com/NgZhengHan/extract-email-from-http-header',
     author='Ng Zheng Han',
